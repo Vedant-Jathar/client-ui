6696ad767c09292130d18961f026f7d220a1b674
Vedant_Jathar <jatharvedant16@gmail.com>
2025-07-18 14:31:06 +0530

    next js and shadcn setup, font and background color set, building the navbar

---
+++ src/app/(Home)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-6xl container">
+        <h1>Hello there</h1>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import Header from "@/components/custom/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const manrope = Manrope({
+  variable: "--font-manrope",
   subsets: ["latin"],
 });
 
@@ -25,8 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          'min-h-screen bg-background font-manrope antialiased',
+          manrope.variable
+        )}
       >
+        <Header />
         {children}
       </body>
     </html>
