9078ab7408bae31a931d997503ebe1dd473acca2
Vedant_Jathar <jatharvedant16@gmail.com>
2025-08-11 00:00:04 +0530

    Add Suspense boundary to cartIems comp

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import CartItems from './components/cart-items'
 
 const Cart = () => {
-    
+
     return (
         <>
             <section>
                 <div className='container mx-auto py-6'>
                     <h1 className='font-semibold text-2xl'>Shopping cart</h1>
                     <div className='bg-white rounded-lg p-6 mt-6'>
-                        <CartItems />
+                        <Suspense fallback={<p>Loading cart items...</p>}>
+                            <CartItems />
+                        </Suspense>
                     </div>
                 </div>
             </section>
